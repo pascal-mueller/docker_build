678dbff8d236bb14ae18456d8966f6e25520cecd
Pascal Müller <pamuelle@student.ethz.ch>
2022-10-04 10:26:17 +0200

    Created basic express server and added routes to order visitenkarten

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+// For parsing application/json
+app.use(bodyParser.json())
+
+// Landing page
+app.get('/', (req, res) => {
+  res.send('Hallo! Bestelle jetzt deine Visitenkarte.')
+})
+
+// orders/businesscard
+app.post('/orders/businesscard', (req, res) => {
+  data = req.body // ATTENTION: Unvalidated user input!
+  msg = data.brand + " Visitenkarte für " + data.name + " ist bestellt."
+
+  res.send(msg)
+})
+
+// Start server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
